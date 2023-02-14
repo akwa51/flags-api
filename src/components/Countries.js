ac5adaa94fa537998f900ad3b3f02c2140d26f42
akwasi kusi-appiah <akappiah@gmail.com>
2023-02-14 09:38:14 +0000

    creating components and importing data

---
+++ src/components/Countries.js
@@ -0,0 +1,12 @@
+import React from 'react'
+const url='https://restcountries.com/v3.1/all';
+
+const Countries = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Countries
