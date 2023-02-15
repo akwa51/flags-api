f838a4a0304dec388e2baac235cfe6c717f765f6
akwasi kusi-appiah <akappiah@gmail.com>
2023-02-15 16:23:55 +0000

    modifying header styles for media queries

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,8 +8,8 @@ const Header = () => {
                 <h1>Where in the World?</h1> 
             </div>
 
-            <div>
-                <i className='fas-Moon'></i>Dark Mode; 
+            <div className='theme_class'>
+                <i className='fas-Moon'></i>Dark Mode 
             </div>
         </div>
     </>
